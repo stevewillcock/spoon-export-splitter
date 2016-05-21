aaa3793970ea38de55acc9f0107c594b9e070d70
Stephen Willcock <steve@rowanbeach.com>
2016-05-22 00:32:27 +0100

    Extracting function to export nodes, exporting jobs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,33 +1,40 @@
 import java.io.File
 
 import scala.language.postfixOps
+import scala.xml.{NodeSeq, XML}
 
 object Main extends App {
 
-  import scala.xml.XML
-
-  val xmlExportFile = XML.loadFile(this.args(0))
-  val exportDirectory = new File(this.args(1))
-
-  val transformNodes = xmlExportFile \ "transformations" \ "transformation"
-  println(transformNodes.length)
-
   def snakify(name: String) = name.replaceAll("([A-Z]+)([A-Z][a-z])", "$1_$2").replaceAll("([a-z\\d])([A-Z])", "$1_$2").replace(" ", "_").toLowerCase
 
-  transformNodes.toList.foreach { kettleTransformXML =>
+  val xmlExportFile = XML.loadFile(this.args(0))
 
-    val (name, directory) = kettleTransformXML \ "info" match {
-      case infoNode => (infoNode \ "name" text, infoNode \ "directory" text)
+  def exportNodes(nodes: NodeSeq, nameRootFinder: NodeSeq => NodeSeq, fileExtension: String): Unit = {
+
+    println(s"Exporting ${nodes.length} files as $fileExtension")
+
+    nodes.toList.foreach { kettleXMlNode =>
+
+      val (name, directory) = nameRootFinder(kettleXMlNode) match {
+        case n =>
+          (n \ "name" text, n \ "directory" text)
+      }
+
+      val targetDirectory = new File(exportDirectory, snakify(directory))
+      val targetFile = new File(targetDirectory, s"${snakify(name)}.$fileExtension")
+
+      targetDirectory.mkdirs()
+      val path = targetFile.getPath
+      println(s"Saving $name as $path")
+      XML.save(path, kettleXMlNode)
+
     }
 
-    val targetDirectory = new File(exportDirectory, snakify(directory))
-    val targetFile = new File(targetDirectory, snakify(name) + ".ktr")
-
-    targetDirectory.mkdirs()
-    val path = targetFile.getPath
-    println(s"Saving $name as $path")
-    XML.save(path, kettleTransformXML)
-
   }
+
+  val exportDirectory = new File(this.args(1))
+
+  exportNodes(xmlExportFile \ "transformations" \ "transformation", n => n \ "info", "ktr")
+  exportNodes(xmlExportFile \ "jobs" \ "job", n => n, "kjb")
 
 }
